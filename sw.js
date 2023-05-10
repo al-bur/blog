e0a5aed46848d2573be26d310786e69418a4758d
al-bur <64825713+al-bur@users.noreply.github.com>
2023-05-10 09:02:23 +0000

    Deploying to deploy from @ al-bur/al-bur.github.io@54bbcf4d7426a3dc63c9fe43cb4e168e7a20741f 🚀

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4f3d5cbc58b491960ecf.js"
+    "url": "webpack-runtime-8373377f8189d9aa1bc4.js"
   },
   {
     "url": "styles.6e4fdc2f3b1b83a07768.css"
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-727eca7a674e10b21a9f.js"
   },
   {
-    "url": "app-5ea5b2ee7d47557e0a8b.js"
+    "url": "app-a00852593b8d65cfbbc3.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-38fe08ef875e5952d5f0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c10082eaefe2447ffc85ce7b6fb20c1a"
+    "revision": "f3a68d4183f797811e7846676f15c50a"
   },
   {
     "url": "manifest.webmanifest",
@@ -73,7 +73,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-5ea5b2ee7d47557e0a8b.js`))) {
+  if (!resources || !(await caches.match(`/app-a00852593b8d65cfbbc3.js`))) {
     return await fetch(event.request)
   }
 
